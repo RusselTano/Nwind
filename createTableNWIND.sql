eb15f68c89bfa7f0271d834b5cf3efd5c6841de3
Dylane <tanolamene@gmail.com>
2024-10-26 04:13:11 -0300

    Creation de la table Categories

--- createTableNWIND.sql
+++ createTableNWIND.sql
@@ -1,2 +1,15 @@
 /****************************************** Nwind ***********************************/
+-- Categories
+
+CREATE SEQUENCE CategoriesID_seq
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 200;
+
+CREATE TABLE Categories(
+    CategoryID NUMBER 
+        CONSTRAINT Categories_pk PRIMARY KEY,
+    CategoryName VARCHAR2(40)
+        CONSTRAINT Categories_CategoryName_nn NOT NULL
+);
 
